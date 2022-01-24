a1d3fb8d1694d6e26e32fac229a1e961da135d7d
Matt Mueller <54808875+matthewlouismueller@users.noreply.github.com>
2022-01-23 21:46:18 -0500

    Stashed changes

--- MonitorControl.py
+++ MonitorControl.py
@@ -76,12 +76,20 @@ serial_write(b'display.show(Image.HAPPY)')
 
 try:
     try:
-        exec(open('../apikeys.py').read())
-        base = 'https://cad.onshape.com'
-        client = Client(configuration={"base_url": base,
-                                    "access_key": access,
-                                    "secret_key": secret})
-        print('client configured')
+        try:
+            exec(open('../apikeys.py').read())
+            base = 'https://cad.onshape.com'
+            client = Client(configuration={"base_url": base,
+                                        "access_key": access,
+                                        "secret_key": secret})
+            print('client configured')
+        except:
+            exec(open('../../apikeys.py').read())
+            base = 'https://cad.onshape.com'
+            client = Client(configuration={"base_url": base,
+                                        "access_key": access,
+                                        "secret_key": secret})
+            print('client configured')
     except:
         exec(open('apikeys.py').read())
         base = 'https://cad.onshape.com'
@@ -90,37 +98,20 @@ try:
                                     "secret_key": secret})
         print('client configured')
 except:
-    keyConfig = input('api keys not found, would you like to import keys from a file? [y/n]: ')
-    if keyConfig == "y":
-        root = tk.Tk()
-        root.withdraw()
-        root.update()
-        file_path = filedialog.askopenfilename()
-        exec(open(file_path).read())
-        base = 'https://cad.onshape.com'
-        client = Client(configuration={"base_url": base,
-                                    "access_key": access,
-                                    "secret_key": secret})
-        print('client configured')
-        # root.deiconify()
-        root.update()
-        test = root.destroy()
-        print(test)
-        # root.quit()
-    else:
-        access = input('Please enter your access key: ')
-        secret = input('Please enter your secret key: ')
-        base = 'https://cad.onshape.com'
-        client = Client(configuration={"base_url": base,
-                                    "access_key": access,
-                                    "secret_key": secret})
-        print('client configured')
+    print('Api keys file not found. Input keys manually.')
+    access = input('Please enter your access key: ')
+    secret = input('Please enter your secret key: ')
+    base = 'https://cad.onshape.com'
+    client = Client(configuration={"base_url": base,
+                                "access_key": access,
+                                "secret_key": secret})
+    print('client configured')
 
 # print()
 url = str(input('What is the url of your Onshape assembly? (paste URL then press enter twice): '))
 
 ## Bug - url input does not continue after copy paste. placeholder fix for now
-placeholder = input()
+# placeholder = input()
 
 print(serial_write(b'accelerometer.get_x()'))
 
